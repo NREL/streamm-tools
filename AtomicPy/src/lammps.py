971fe0304d72769e432535e0a3a3d73bfb62ea81
Travis Kemper <travis.kemper@nrel.gov>
2014-05-16 12:01:45 -0600

    add lammps data file read in

--- AtomicPy/src/lammps.py
+++ AtomicPy/src/lammps.py
@@ -28,10 +28,10 @@ def lmp_types(ELN,ATYPE,AMASS,BONDS,ANGLES,DIH):
     #
     # Count atom types
     #
-    NA = len(ATYPE)
+    n_atoms = len(ATYPE)
     TYPE_CNT = -1 # so array is 0-N
     debug = 0
-    for i in range(NA):
+    for i in range(n_atoms):
         new_type = 1
         for ind in range( len(ATYPE_REF) ):
             if( ATYPE[i] == ATYPE_REF[ind] ):
@@ -227,7 +227,7 @@ def lmp_types(ELN,ATYPE,AMASS,BONDS,ANGLES,DIH):
         
     debug = 0
     if( debug ):
-        for i in range(NA):
+        for i in range(n_atoms):
             print i,ATYPE[i],ATYPE_IND[i]
 
         print len( ATYPE_REF), len(ATYPE_MASS)
@@ -250,6 +250,360 @@ def lmp_types(ELN,ATYPE,AMASS,BONDS,ANGLES,DIH):
 
     return ATYPE_IND , ATYPE_REF,  ATYPE_MASS ,BTYPE_IND , BTYPE_REF, ANGTYPE_IND , ANGTYPE_REF, DTYPE_IND , DTYPE_REF 
 
+
+
+def read_data(data_file):
+
+    # atomicpy functions
+    import file_io
+    import sys, numpy
+    
+    
+    debug = 0
+    verbose = 1
+
+    if( file_io.file_exists(data_file )):
+        if( debug ): print "Reading in ",data_file
+    else:
+        print " Specified file ",data_file," does not exisit "
+        sys.exit("Invalid file ")
+        
+
+    F = open(data_file , 'r' )
+    lines = F.readlines()
+    F.close()
+    
+    # initialize variables
+    LAT_CONST = numpy.zeros( (3,3) )
+    
+    #
+    # Read in data header with number of parameters 
+    #
+    for line in lines:
+        col = line.split()
+        if ( len(col) >=2 ):
+            # Read in number of each topolgical component  
+            if( col[1] == "atoms" ):
+                n_atoms = int( col[0] )
+            elif( col[1] == "bonds" ):
+                n_bonds = int( col[0] )
+            elif( col[1] == "angles" ):
+                n_angles = int( col[0] )
+            elif( col[1] == "dihedrals" ):
+                n_dihedrals = int( col[0] )
+            elif( col[1] == "impropers" ):
+                n_impropers = int( col[0] )
+
+        if ( len(col) >= 3 ):
+            # Read in number of each parameter type 
+            if( col[1] == "atom" and   col[2] == "types" ):
+                n_atypes = int( col[0] )
+            elif( col[1] == "bond" and   col[2] == "types"  ):
+                n_btypes = int( col[0] )
+            elif( col[1] == "angle" and   col[2] == "types"  ):
+                n_angtypes = int( col[0] )
+            elif( col[1] == "dihedral" and   col[2] == "types"  ):
+                n_dtypes = int( col[0] )
+            elif( col[1] == "improper" and   col[2] == "types"  ):
+                n_imptypes = int( col[0] )
+                
+        # Read in box size    
+        if ( len(col) >= 4 ):
+            if( col[2]  == "xlo"  and col[2]  == "xhi"  ):
+                LAT_CONST[0,0] = float( col[1] ) - float( col[0] ) 
+            if( col[2]  == "ylo"  and col[2]  == "yhi"  ):
+                LAT_CONST[1,1] = float( col[1] ) - float( col[0] ) 
+            if( col[2]  == "zlo"  and col[2]  == "zhi"  ):
+                LAT_CONST[2,2] = float( col[1] ) - float( col[0] )
+
+    # Prind debug
+    if( verbose ):
+        print " atoms ",n_atoms
+        print " n_bonds ",n_bonds
+        print " n_angles ",n_angles
+        print " n_dihedrals ",n_dihedrals
+        print ""
+        print "n_atom_types",n_atypes
+        print "n_bond_types",n_btypes
+        print "n_angle_types",n_angtypes
+        print "n_dihedral_types",n_dtypes
+        
+    #
+    # Intialize
+    #   - read in boolean to off
+    #
+    read_Masses = 0
+    read_Pair = 0
+    read_Bond_coeff = 0
+    read_Angle_coeff = 0
+    read_Dihedral_coeff = 0
+    read_Improper_coeff = 0
+    
+    read_Atoms = 0
+    read_Bonds = 0
+    read_Angles = 0
+    read_Dihedrals = 0
+    read_Impropers = 0
+    
+    #   - lists as indecise can be out of order in data file
+    ATYPE_REF = n_atypes*[""]
+    ATYPE_MASS = numpy.zeros(n_atypes)
+    ATYPE_EP = numpy.zeros(n_atypes)
+    ATYPE_SIG = numpy.zeros(n_atypes)
+    
+    BTYPE_REF = n_btypes*[2*[""]]
+    BONDTYPE_R0 = numpy.zeros(n_btypes)
+    BONDTYPE_K = numpy.zeros(n_btypes)
+    
+    ANGTYPE_REF = n_angtypes*[3*[""]]
+    ANGLETYPE_R0 = numpy.zeros(n_angtypes)
+    ANGLETYPE_K = numpy.zeros(n_angtypes)
+    
+    DTYPE_REF = n_dtypes*[4*[""]]
+    DIHTYPE_C = numpy.zeros((n_dtypes,4))
+    DIHTYPE_F = numpy.zeros(n_dtypes)
+    DIHTYPE_K = numpy.zeros(n_dtypes)
+    DIHTYPE_PN = numpy.zeros(n_dtypes)
+    DIHTYPE_PHASE = numpy.zeros(n_dtypes)
+    
+    
+    RESN = n_atoms*[0]
+    ATYPE_IND  = n_atoms*[0]
+    CHARGES  = numpy.zeros(n_atoms)
+    R = n_atoms*[numpy.zeros(3)]
+    ATYPE  = n_atoms*[""]
+    
+    BONDS = n_bonds*[[0,0]]
+    BTYPE_IND = n_bonds*[0]
+    
+    ANGLES = n_angles*[[0,0,0]]
+    ANGTYPE_IND = n_angles*[0]
+    
+    DIH = n_dihedrals*[[0,0,0,0]]
+    DTYPE_IND = n_dihedrals*[0]
+    
+    
+    #
+    # Read in data parameters 
+    #
+    
+    for line in lines:
+        col = line.split()
+        if ( len(col) >= 1  ):
+            if( col[0] == "Masses" ):
+                read_Masses = 1
+                cnt_Masses = 0
+                
+            if( col[0] == "Atoms" ):
+                read_Atoms = 1
+                cnt_Atoms = 0
+                
+            if( col[0] == "Bonds" ):
+                read_Bonds = 1
+                cnt_Bonds = 0
+                
+            if( col[0] == "Angles" ):
+                read_Angles = 1
+                cnt_Angles  = 0
+                
+            if( col[0] == "Dihedrals" ):
+                read_Dihedrals = 1
+                cnt_Dihedrals = 0
+                
+            if( col[0] == "Impropers" ):
+                read_Impropers = 1
+                cnt_Impropers = 0
+                
+        if ( len(col) >= 2 ):
+            if( col[0] == "Pair" and col[1] == "Coeffs" ):
+                read_Pair = 1
+                cnt_Pair = 0
+            if( col[0] == "Bond" and col[1] == "Coeffs" ):
+                read_Bond_coeff = 1
+                cnt_Bond_coeff = 0
+            if( col[0] == "Angle" and col[1] == "Coeffs" ):
+                read_Angle_coeff  = 1
+                cnt_Angle_coeff = 0
+            if( col[0] == "Dihedral" and col[1] == "Coeffs" ):
+                read_Dihedral_coeff  = 1
+                cnt_Dihedral_coeff  = 0
+                
+            if( col[0] == "Improper" and col[1] == "Coeffs" ):
+                read_Improper_coeff  = 1
+                cnt_Improper_coeff  = 0
+                
+        if( read_Masses and  len(col) >= 2 ):
+            cnt_Masses += 1
+            ind = int(col[0]) - 1
+            ATYPE_MASS[ind] = float(col[1])
+            if(cnt_Masses ==  n_atypes ):
+                read_Masses = 0
+            if( len(col) >= 4 ):
+                ATYPE_REF[ind] = col[3]
+            else:
+                ATYPE_REF[ind] = "??"
+
+        if( read_Pair and  len(col) >= 3 ):
+            cnt_Pair += 1
+            ind = int(col[0]) - 1
+            ATYPE_EP[ind] = float(col[1])
+            ATYPE_SIG[ind] = float(col[2])
+            if(cnt_Pair >=  n_atypes ):
+                read_Pair = 0
+        
+        if( read_Bond_coeff and  len(col) >= 3 ):
+            cnt_Bond_coeff += 1
+            #AT_i = int(col[0])
+            #AT_j = int(col[1])
+            b_ind = int( col[0]) - 1
+            if( b_ind > n_btypes ):
+                print sys.exit(" Error in data file index of bond parameter exceeds number of bond parameters specified with bond types ")
+                
+            BTYPE_REF[b_ind][0] = "??"
+            BTYPE_REF[b_ind][1] = "??"
+            BONDTYPE_K[b_ind] = float(col[1])
+            BONDTYPE_R0[b_ind] = float(col[2])
+            
+            if( cnt_Bond_coeff >=  n_btypes ):
+                read_Bond_coeff = 0
+                
+        
+        if( read_Angle_coeff and  len(col) >= 3 ):
+            cnt_Angle_coeff += 1
+            #AT_i = int(col[0])
+            #AT_j = int(col[1])
+            a_ind = int( col[0]) - 1
+            if( a_ind > n_angtypes ):
+                print sys.exit(" Error in data file index of angle parameter exceeds number of angle parameters specified with angle types ")
+                
+            ANGTYPE_REF[a_ind][0] = "??"
+            ANGTYPE_REF[a_ind][1] = "??"
+            ANGTYPE_REF[a_ind][2] = "??"
+            ANGLETYPE_K[a_ind] = float(col[1])
+            ANGLETYPE_R0[a_ind] = float(col[2])
+            
+            if( cnt_Angle_coeff >=  n_angtypes ):
+                read_Angle_coeff = 0
+                
+        
+        
+        if( read_Dihedral_coeff and  len(col) >= 3 ):
+            cnt_Dihedral_coeff += 1
+            #AT_i = int(col[0])
+            #AT_j = int(col[1])
+            d_ind = int( col[0]) - 1
+            
+            if( debug): print " reading dih type ",d_ind," cnt ",cnt_Dihedral_coeff," of ",n_dtypes
+            
+            if( d_ind > n_dtypes ):
+                print sys.exit(" Error in data file index of dihedral parameter exceeds number of dihedral parameters specified with dihedral types ")
+                
+            DTYPE_REF[d_ind][0] = "??"
+            DTYPE_REF[d_ind][1] = "??"
+            DTYPE_REF[d_ind][2] = "??"
+            DTYPE_REF[d_ind][3] = "??"
+            
+            # Assume OPLS dihedral type
+            DIHTYPE_F[d_ind] = 3
+            DIHTYPE_C[d_ind][0] = float(col[1])
+            DIHTYPE_C[d_ind][1] = float(col[2])
+            DIHTYPE_C[d_ind][2] = float(col[3])
+            DIHTYPE_C[d_ind][3] = float(col[4])
+            
+            if( cnt_Dihedral_coeff >=  n_dtypes ):
+                read_Dihedral_coeff = 0
+                
+        if(read_Atoms and len(col) >= 7 ):
+            cnt_Atoms += 1
+            ind = int( col[0]) - 1
+            if( ind > n_atoms ):
+                print sys.exit(" Error in data file index of atoms exceeds number of atoms specified with atoms ")
+                
+            RESN[ind] = int(col[1])
+            ATYPE_IND[ind]  = int(col[2]) - 1
+            CHARGES[ind]  = float(col[3])
+            R[ind] = numpy.array( [ float(col[4]),float(col[5]),float(col[6])])
+            ATYPE[ind]  = "??"
+        
+            if( cnt_Atoms >=  n_atoms ):
+                read_Atoms = 0
+
+        if(read_Bonds and len(col) >= 4 ):
+            cnt_Bonds += 1
+            ind = int( col[0]) - 1
+
+            if( ind > n_bonds ):
+                print sys.exit(" Error in data file index of bonds exceeds number of bonds specified with bonds ")
+                
+            BTYPE_IND[ind] = int(col[1] ) - 1
+            BONDS[ind] = [int(col[2]) - 1 , int(col[3]) - 1 ]
+            if( cnt_Bonds >=  n_bonds ):
+                read_Bonds = 0
+                
+            
+        if(read_Angles and len(col) >= 5 ):
+            cnt_Angles += 1
+            ind = int( col[0]) - 1
+            ANGTYPE_IND[ind] = int(col[1] ) - 1
+            ANGLES[ind] = [int(col[2]) - 1, int(col[3]) - 1, int(col[4]) - 1 ]
+            if( cnt_Angles >=  n_angles ):
+                read_Angles = 0
+                
+            
+        if(read_Dihedrals and len(col) >= 6 ):
+            cnt_Dihedrals += 1
+            ind = int( col[0]) - 1
+            
+            DTYPE_IND[ind] = int(col[1] ) - 1
+            DIH[ind] = [int(col[2]) - 1,int(col[3]) - 1, int(col[4]) - 1,int(col[5]) - 1]
+            if( read_Dihedrals >=  n_dihedrals ):
+                read_Dihedrals = 0
+                
+        #    
+        #if(read_Impropers and len(col) >= 2 ):
+        #    cnt_Bonds += 1
+        #    ind = int( col[0]) - 1
+        #    BTYPE_IND[ind] = int(col[1] ) - 1
+        #    BONDS[ind][0] = int(col[2])
+        #    if( cnt_Bonds >=  n_atoms ):
+        #        read_Bonds = 0
+        #        
+            
+            
+        
+    if( debug):
+        for ind in range(len(ATYPE_MASS)):
+            print ind+1,ATYPE_MASS[ind]
+            
+        for ind in range(len(ATYPE_EP)):
+            print ind+1,ATYPE_EP[ind],ATYPE_SIG[ind]
+        
+        for ind in range(n_btypes):
+            print ind+1,BONDTYPE_R0[ind],BONDTYPE_K[ind]
+                
+        for ind in range(n_angtypes):
+            print ind+1,ANGLETYPE_R0[ind],ANGLETYPE_K[ind]
+        
+        for ind in range(n_dtypes):
+            print ind+1,DIHTYPE_C[ind]
+        
+    debug =0
+    
+    if( debug):
+        for ind in range(len(BONDS)):
+            print ind+1,BONDS[ind]
+            
+    if(debug):
+        sys.exit("debug 1 ")
+    #
+    #      
+    return (ATYPE_REF,ATYPE_MASS,ATYPE_EP,ATYPE_SIG,
+            BTYPE_REF,BONDTYPE_R0,BONDTYPE_K,
+            ANGTYPE_REF,ANGLETYPE_R0,ANGLETYPE_K,
+            DIH,DTYPE_IND,DTYPE_REF,DIHTYPE_F,DIHTYPE_K,DIHTYPE_PN,DIHTYPE_PHASE,DIHTYPE_C,RESN,
+            ATYPE_IND,CHARGES,R , ATYPE, BONDS ,BTYPE_IND, ANGLES ,ANGTYPE_IND, LAT_CONST)
+    #
+
 def print_lmp(data_file, ATYPE_REF,ATYPE_MASS,ATYPE_EP,ATYPE_SIG,
               BTYPE_REF,BONDTYPE_R0,BONDTYPE_K,
               ANGTYPE_REF,ANGLETYPE_R0,ANGLETYPE_K,
@@ -264,11 +618,11 @@ def print_lmp(data_file, ATYPE_REF,ATYPE_MASS,ATYPE_EP,ATYPE_SIG,
     #
 
     # Calculate totals
-    NA = len( ATYPE )
-    nbonds = int(len(BONDS))
-    nangles = int(len(ANGLES))
-    ndih = int(len(DIH))
-    nimp = 0
+    n_atoms = len( ATYPE )
+    n_bonds = int(len(BONDS))
+    n_angles = int(len(ANGLES))
+    n_dihedrals = int(len(DIH))
+    n_impropers = 0
     n_atypes = int(len( ATYPE_REF)) #+ 1
     n_btypes = int(len( BTYPE_REF)) #+ 1
     n_angtypes = int(len( ANGTYPE_REF)) #+ 1
@@ -285,11 +639,11 @@ def print_lmp(data_file, ATYPE_REF,ATYPE_MASS,ATYPE_EP,ATYPE_SIG,
     F = open( data_file, 'w' )
     F.write('  Lammps data file \n')
     F.write('\n')
-    F.write( "%10d  atoms \n" % NA )
-    F.write( "%10d  bonds \n" %  nbonds )
-    F.write( "%10d  angles \n" % nangles )
-    F.write( "%10d  dihedrals \n" %  ndih )
-    F.write( "%10d  impropers \n" % nimp  )
+    F.write( "%10d  atoms \n" % n_atoms )
+    F.write( "%10d  bonds \n" %  n_bonds )
+    F.write( "%10d  angles \n" % n_angles )
+    F.write( "%10d  dihedrals \n" %  n_dihedrals )
+    F.write( "%10d  impropers \n" % n_impropers  )
     F.write('\n')
     F.write( "%10d  atom types \n" % n_atypes  )
     F.write( "%10d  bond types \n" % n_btypes )
@@ -322,8 +676,8 @@ def print_lmp(data_file, ATYPE_REF,ATYPE_MASS,ATYPE_EP,ATYPE_SIG,
     for b_ind in range(  len( BTYPE_REF )  ):
         AT_i =  BTYPE_REF[b_ind][0]
         AT_j =  BTYPE_REF[b_ind][1]
-        r_0 = BONDTYPE_R0[b_ind]
         k = BONDTYPE_K[b_ind]
+        r_0 = BONDTYPE_R0[b_ind]
         F.write( "%10d %12.6f %12.6f # %5s %5s  \n" % (b_ind + 1, k , r_0, AT_i, AT_j ) )
     F.write('\n')
     F.write(' Angle Coeffs \n')
@@ -375,7 +729,7 @@ def print_lmp(data_file, ATYPE_REF,ATYPE_MASS,ATYPE_EP,ATYPE_SIG,
     F.write(' Atoms \n')
     F.write('\n')
     TOTAL_CHARGE = 0.0
-    for i in range(NA):
+    for i in range(n_atoms):
         F.write( "%9d %9d %8d %12.8f %12.6f %12.6f %12.6f # %5s \n" % (i+1,RESN[i],ATYPE_IND[i]+1,CHARGES[i],R[i][0],R[i][1],R[i][2] , ATYPE[i] )  )
         TOTAL_CHARGE = TOTAL_CHARGE + float( CHARGES[i] )
         
@@ -391,7 +745,8 @@ def print_lmp(data_file, ATYPE_REF,ATYPE_MASS,ATYPE_EP,ATYPE_SIG,
         AT_i =  ATYPE[a_i]
         AT_j =  ATYPE[a_j]
         #
-        b_ind = BTYPE_IND[i]  + 1 
+        b_ind = BTYPE_IND[i]  + 1
+        
         F.write(  '%9d %8d %9d %9d # %5s %5s \n' % (i+1,b_ind,a_i+1,a_j+1 , AT_i, AT_j ) )
     F.write('\n')
     F.write(' Angles \n')
@@ -643,14 +998,14 @@ def last_xmol(xmol_file,options):
     F.close()
     
     col = Lines[0].split()
-    NA = int( col[0] ) 
+    n_atoms = int( col[0] ) 
 
     R = []
     line_cnt = 0 
     for line in Lines :
         col = line.split()
         line_cnt = line_cnt + 1
-        if( line_cnt > (NA + 2 ) ):
+        if( line_cnt > (n_atoms + 2 ) ):
             line_cnt = 1
             R = []
             if( debug ): print " restarting ", line
