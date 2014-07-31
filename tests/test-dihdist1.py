290f78f1acec730f8a46bdd473ae3053d3536606
Travis Kemper <travis.kemper@nrel.gov>
2014-07-31 16:10:58 -0600

    add test-rdf and fix parentheses in test-dihedral

--- tests/test-dihdist1.py
+++ tests/test-dihdist1.py
@@ -4,7 +4,7 @@ import os
 
 set_path="PYTHONPATH=../src/:../AtomicPy/src/"
 os.system(set_path)
-run_py="python ../scripts/dihdist.py -j acc1_D1_R2R200_A2_R3_n1_R41n1R41n1R40n1__n5.json  --frame_o 0      --frame_step 1    --in_lammpsxyz n5.xyz   --fftype_k "S"  --fftype_i 'C!'  --fftype_j 'C!' --fftype_l 'S' -v "
+run_py="python ../scripts/dihdist.py -j acc1_D1_R2R200_A2_R3_n1_R41n1R41n1R40n1__n5.json  --frame_o 0      --frame_step 1    --in_lammpsxyz n5.xyz   --fftype_k 'S'  --fftype_i 'C!'  --fftype_j 'C!' --fftype_l 'S' -v "
 os.system(run_py)
 
 
