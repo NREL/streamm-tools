20de206fe13ce0e71199eda8a053b30a600ff5f9
Scott <Scott.Sides@nrel.gov>
2014-08-04 17:38:08 -0600

    commenting out path setting code

--- tests/test-rdf.py
+++ tests/test-rdf.py
@@ -3,7 +3,7 @@
 import os
 
 set_path="PYTHONPATH=../src/:../AtomicPy/src/"
-os.system(set_path)
+# os.system(set_path)
 run_py="python ../scripts/rdf.py  -v -j acc1_D1_R2R200_A2_R3_n1_R41n1R41n1R40n1__n5.json   --in_lammpsxyz  n5.xyz   --fftype_i 'C!'   --fftype_j 'C!'  -o test_rdf "
 
 os.system(run_py)
