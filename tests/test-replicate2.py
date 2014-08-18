fae35bca6a6cbc740164922928670bd159d91ead
Scott <Scott.Sides@nrel.gov>
2014-08-18 11:32:41 -0600

    Making a version of one of Travis' replicate test with the 'fixed_rnd_seed' flag on so it can be added to tests
    Adding results for this test

---
+++ tests/test-replicate2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+
+set_path="PYTHONPATH=../src/:../AtomicPy/src/"
+# os.system(set_path)
+# run_py="python ../scripts/replicate.py --gro SOL.gro --top SOL.top   --sol_gro SOL.gro   --sol_top SOL.top   --den_target 0.1  --atoms_target 200    --perc_sol 50 -v "
+run_py="python ../scripts/replicate.py --fixed_rnd_seed --gro SOL.gro --top SOL.top   --sol_gro SOL.gro   --sol_top SOL.top   --den_target 0.1  --atoms_target 200    --perc_sol 50 -v "
+os.system(run_py)
