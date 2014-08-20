fix pbc error in dihedral angle calculation

src/pbcs.py
@@ -11,12 +11,8 @@ Vector opperations for particles in a box
 
 import numpy as np
 
+def delta_r_c(r_i,r_j,latticevec):
 
-def sq_drij_c(r_i,r_j,latticevec):
-    """
-    Reutrn square magnitude of distance between to vectors  using cubic periodic boundry conditions 
-    """
-    
     # Find magnitude of dr_ij
     r_ij  = r_j - r_i
     
@@ -25,6 +21,15 @@ def sq_drij_c(r_i,r_j,latticevec):
     r_z = r_ij[2] - latticevec[2][2] * round( r_ij[2]/  latticevec[2][2] )
     
     dr_pbc = np.array( [r_x,r_y,r_z] )
+
+    return dr_pbc
+
+def sq_drij_c(r_i,r_j,latticevec):
+    """
+    Reutrn square magnitude of distance between to vectors  using cubic periodic boundry conditions 
+    """
+
+    dr_pbc = delta_r_c(r_i,r_j,latticevec)
     
     sq_dr = np.dot( dr_pbc,dr_pbc)
     
@@ -32,11 +37,16 @@ def sq_drij_c(r_i,r_j,latticevec):
     
     
 
-def norm_r_ij(r_i,r_j):
+def norm_r_ij(r_i,r_j,latticevec):
     """
     Normailze difference between two vectors 
     """
-    delta_ij = r_j - r_i
+    
+    delta_ij = delta_r_c(r_i,r_j,latticevec)
+    
+    print "delta_ij ",delta_ij
+    print " mag ",np.linalg.norm(delta_ij)
+    
     return (delta_ij)/np.linalg.norm(delta_ij)
 
 def getAngle(r_i,r_j):
@@ -49,7 +59,7 @@ def getAngle(r_i,r_j):
     """
     #
 
-    r_i_norm = r_i/np.linalg.norm(r_i)
+    r_i_norm = r_i/np.linalg.norm(r_i,)
     r_j_norm = r_j/np.linalg.norm(r_j) 
     dot_ij = np.dot(r_i_norm,r_j_norm)
     

src/structureContainer.py
@@ -1600,21 +1600,40 @@ class StructureContainer:
                        
         
         """
-
+        debug = False
+        
         r_k = np.array( [ float( self.ptclC[a_k].position[0] ),float( self.ptclC[a_k].position[1] ),float( self.ptclC[a_k].position[2] ) ] )
         r_i = np.array( [ float( self.ptclC[a_i].position[0] ),float( self.ptclC[a_i].position[1] ),float( self.ptclC[a_i].position[2] ) ] )
         r_j = np.array( [ float( self.ptclC[a_j].position[0] ),float( self.ptclC[a_j].position[1] ),float( self.ptclC[a_j].position[2] ) ] )
         r_l = np.array( [ float( self.ptclC[a_l].position[0] ),float( self.ptclC[a_l].position[1] ),float( self.ptclC[a_l].position[2] ) ] )
 
-        v1 = pbcs.norm_r_ij(r_k, r_i)
-        v2 = pbcs.norm_r_ij(r_i, r_j)
-        v3 = pbcs.norm_r_ij(r_j, r_l)
+        if( debug ):
+            print " lat vec ",self.latvec
+            print "r_k ",r_k
+            print "r_i ",r_i
+            print "r_j ",r_j
+            print "r_l ",r_l
+
+        v1 = pbcs.norm_r_ij(r_k, r_i,self.latvec)
+        v2 = pbcs.norm_r_ij(r_i, r_j,self.latvec)
+        v3 = pbcs.norm_r_ij(r_j, r_l,self.latvec)
 
         v1v2 = np.cross(v1,v2)
         v2v3 = np.cross(v2,v3)
 
 
         angle_i = pbcs.getAngle(v1v2,v2v3)
+
+        
+        if( debug):
+            print "v1 ",v1
+            print "v2 ",v2
+            print "v3 ",v3
+            print "v1v2 ",v1v2
+            print "v2v3 ",v2v3
+            print "angle_i ",angle_i
+
+
         #
         # Find sign of angle 
         #