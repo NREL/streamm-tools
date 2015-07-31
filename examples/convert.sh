6778e67ec53ea1f13a16b10984ff255666ca4e6f
tkemper <travis.kemper@nrel.gov>
2015-07-31 11:17:56 -0600

    Add exmaple convert.sh com2xyz.py - translate gaussian .com to xyz com2gro.py - translate gaussian .com to .gro com2gromacs.py - translate gaussian .com to .gro .itp .top for GROMACS runs com2data.py - translate gaussian .com to .data for LAMMPS runs

--- examples/convert.sh
+++ examples/convert.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Convert .com gaussain files into file types for other software 
+MOLDIR=mols/thiophene_R_methane_/
+MOLID=acc1_thiophene_R_methane__n1
 
-python com2xyz.py --in_com mols/thiophene_R_methane_/acc1_thiophene_R_methane__n1.com --out_xyz t.xyz
-python com2gro.py --in_com mols/thiophene_R_methane_/acc1_thiophene_R_methane__n1.com --out_gro t.gro 
+python com2xyz.py --in_com ${MOLDIR}${MOLID}.com --out_xyz ${MOLDIR}${MOLID}.xyz
+python com2gro.py --in_com ${MOLDIR}${MOLID}.com --out_gro ${MOLDIR}${MOLID}.gro 
+python com2gromacs.py --in_com ${MOLDIR}${MOLID}.com  --out_gro ${MOLDIR}${MOLID}.gro --in_itp conj.itp --out_itp ${MOLDIR}${MOLID}.itp --out_top ${MOLDIR}${MOLID}.top 
+python com2data.py --in_com ${MOLDIR}${MOLID}.com  --out_data ${MOLDIR}${MOLID}.data
+
