c1c7e47f4b7e47de22a5f09d2654a0e9022de3a5
tkemper <travis.kemper@nrel.gov>
2015-07-31 12:01:27 -0600

    Modify convert.sh to make a local itp file then move that file

--- examples/convert.sh
+++ examples/convert.sh
@@ -6,6 +6,11 @@ MOLID=acc1_thiophene_R_methane__n1
 
 python com2xyz.py --in_com ${MOLDIR}${MOLID}.com --out_xyz ${MOLDIR}${MOLID}.xyz
 python com2gro.py --in_com ${MOLDIR}${MOLID}.com --out_gro ${MOLDIR}${MOLID}.gro 
-python com2gromacs.py --in_com ${MOLDIR}${MOLID}.com  --out_gro ${MOLDIR}${MOLID}.gro --in_itp conj.itp --out_itp ${MOLDIR}${MOLID}.itp --out_top ${MOLDIR}${MOLID}.top 
-python com2data.py --in_com ${MOLDIR}${MOLID}.com  --out_data ${MOLDIR}${MOLID}.data
+python com2gromacs.py --in_com ${MOLDIR}${MOLID}.com  --out_gro ${MOLDIR}${MOLID}.gro --in_itp conj.itp --out_itp ${MOLID}.itp --out_top ${MOLDIR}${MOLID}.top 
+mv ${MOLID}.itp ${MOLDIR}
+
+python com2data.py --in_com ${MOLDIR}${MOLID}.com  --in_itp conj.itp --out_data ${MOLDIR}${MOLID}.data
+
+# python replicate_data.py  --in_data ${MOLDIR}${MOLID}.data --out_data 
+
 
