a73eeecf5e5751b77271b9234de69f1d9413d984
tkemper <travis.kemper@nrel.gov>
2015-07-31 13:59:27 -0600

    Add script to run all the molecular generation examples at once

---
+++ examples/mol_gen.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Examples of running the donoracceptorsystems.py script to build complex molecules 
+
+# Files  and directories
+# - donoracceptorsystems.py 
+# Concatenates molecular cply files based on connectivity tags 
+# 
+# - BuildingBlocks
+# 
+#   Must contain the directories 
+# 
+#   -  acceptors   
+#    -   donors            
+#    - functional_groups spacers           
+#    - terminals
+
+source mol_gen_setup.sh 
+
+echo "  Proceed to example 1 [y]yes [n]no"
+read -e ans
+# Example 1
+if [ $ans == "y" ]; then
+    source mol_gen_ex1.sh 
+fi
+
+
+echo "  Proceed to example 2 [y]yes [n]no"
+read -e ans
+# Example 2
+if [ $ans == "y" ]; then
+    source mol_gen_ex2.sh 
+fi
+
+echo "  Proceed to example 3 [y]yes [n]no"
+read -e ans
+# Example 2
+if [ $ans == "y" ]; then
+    source mol_gen_ex3.sh 
+fi
+
+echo "mol_gen finished"
+exit
