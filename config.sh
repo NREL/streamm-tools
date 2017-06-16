84dec45dae3790eaa31a5055b6bd57bcafeb22a1
Scott Sides <swsides@phoenix.txcorp.com>
2017-06-16 14:26:14 -0600

    Changing output in config.sh setup script. Testing commits to branch webchem. This is a new branch for any changes related to WebChem SBIR project.

--- config.sh
+++ config.sh
@@ -13,16 +13,18 @@ echo " "
 
 #
 # Cleaning environment of previous STREAMM related paths
-# 
+#
 $TOP_PATH/src/cleanpath.py
 echo " "
 source clean-PYTHONPATH.sh
 source clean-PATH.sh
+echo ""
+echo "Removing clean-*.sh scripts"
 rm -rf clean-*PATH.sh
 
 #
 # Set PYTHONPATH, PATH  and TOOLS_PATH
-# 
+#
 PYTHONPATH=$TOP_PATH/src:$PYTHONPATH
 PYTHONPATH=./:$PYTHONPATH
 export PYTHONPATH
@@ -37,6 +39,6 @@ echo "Setting PYTHONPATH = $PYTHONPATH"
 echo " "
 echo "Setting PATH       = $PATH"
 echo " "
-echo "NOTE: be sure to source this file to include path in environment"
+echo "NOTE: be sure to have 'source'-ed this file to include paths in environment"
 echo " "
 echo "================================================================================"
