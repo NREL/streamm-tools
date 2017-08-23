8d37a16421ee8371e8d159a86c2634018b89d82a
Kemper <tkemper@tkemper-23195s.nrel.gov>
2017-08-23 13:23:30 -0600

    Try to get install working

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
-#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) Alliance for Sustainable Energy, LLC
+# Distributed under the terms of the Apache License, Version 2.0
 
-from setuptools import setup
+
+import os
+import sys
+import platform
+
+from setuptools import setup, find_packages, Extension
 
 #  packages=['streamm', 'streamm.jobs', 'streamm.jobs', 'streamm.mpi', 'streamm.simulation', 'streamm.structure'],
 # dep = ['os','sys','shutil','time','datetime','json','math','numpy','copy','scipy','random','json','optparse','logging','pickle']
 #  dep openbabel ( py27-openbabel )
 
-setup(name='streamm',
-      version='0.3',
-      url='http://streamm.nrel.gov',
-      author='Travis W. Kemper and Scott W. Sides',
-      author_email='travis.kemper@nrel.gov',
-      test_suite = 'streamm.structure.tests',
-      # data_files=['streamm/periodic_table.json']
-      packages=['streamm'],
-      package_dir={'streamm': 'streamm'},
-      package_data={'streamm': ['periodic_table.json']},
-      #install_requires=['numpy','copy'],
-      )
+setup(
+    name='streamm',
+    version='0.3.1',
+    setup_requires=['numpy', 'setuptools>=18.0'],    
+    url='http://streamm.nrel.gov',
+    author='Travis W. Kemper and Scott W. Sides',
+    author_email='travis.kemper@nrel.gov',
+    
+    test_suite = 'streamm.structure.tests',
+    # data_files=['streamm/periodic_table.json']
+    #packages=['streamm'],
+    packages=find_packages(),
+    
+    # package_dir={'streamm': 'streamm'},
+    # package_data={'streamm': ['periodic_table.json']},
+    #install_requires=['numpy','copy'],
+    )
 
