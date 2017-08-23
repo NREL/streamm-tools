1b4c296c4ace884b2eb3a1fb57b7b4dc9b11b242
Kemper <tkemper@tkemper-23195s.nrel.gov>
2017-08-23 13:47:08 -0600

    add  MANIFEST.in

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 # Copyright (c) Alliance for Sustainable Energy, LLC
 # Distributed under the terms of the Apache License, Version 2.0
 
-
 import os
 import sys
 import platform
@@ -17,15 +16,15 @@ setup(
     name='streamm',
     version='0.3.1',
     setup_requires=['numpy', 'setuptools>=18.0'],    
+    packages=find_packages(),
+    # 
     url='http://streamm.nrel.gov',
     author='Travis W. Kemper and Scott W. Sides',
-    author_email='travis.kemper@nrel.gov',
-    
+    author_email='travis.kemper.w@gmail.com',
+    #
     test_suite = 'streamm.structure.tests',
     # data_files=['streamm/periodic_table.json']
-    #packages=['streamm'],
-    packages=find_packages(),
-    
+    # packages=['streamm'],    
     # package_dir={'streamm': 'streamm'},
     # package_data={'streamm': ['periodic_table.json']},
     #install_requires=['numpy','copy'],
