afe4a947f156c09b0b7f34ad490719ec880d4796
Kemper <tkemper@tkemper-23195s.nrel.gov>
2017-08-24 11:33:35 -0600

    Remove verbose statements from conatianer.py

---
+++ streamm/structure/dihedrals.py
@@ -0,0 +1,68 @@
+# coding: utf-8
+# Copyright (c) Alliance for Sustainable Energy, LLC
+# Distributed under the terms of the Apache License, Version 2.0
+
+from __future__ import division, unicode_literals
+
+__author__ = "Travis W. Kemper, Scott Sides"
+__copyright__ = "Copyright 2015, Alliance for Sustainable Energy, LLC"
+__version__ = "0.3"
+__email__ = "streamm@nrel.gov"
+__status__ = "Beta"
+
+"""
+This module defines the classes relating to the dihedral angles between atoms
+"""
+
+class Dihedral(object):
+    """
+    Data structure for describing any 4-point associatiaon of Particle/s
+    """
+
+    def __init__(self, pkey1, pkey2, pkey3, pkey4):
+        """
+        Constructor for a general dihedral. Checks for types in arguments
+        and throws a TypeError when appropriate
+
+        Args:
+            pkey1   (int)   Index of Particle object in dihedral
+            pkey2   (int)   Index of Particle object in dihedral
+            pkey3   (int)   Index of Particle object in dihedral
+            pkey4   (int)   Index of Particle object in dihedral
+        """
+        
+        if isinstance(pkey1, int):
+            self.pkey1 = pkey1
+        else:
+            raise TypeError("1st arg should be int")
+
+        if isinstance(pkey2, int):
+            self.pkey2 = pkey2
+        else:
+            raise TypeError("2nd arg should be int type")
+
+        if isinstance(pkey3, int):
+            self.pkey3 = pkey3
+        else:
+            raise TypeError("3rd arg should be int type")
+
+        if isinstance(pkey4, int):
+            self.pkey4 = pkey4
+        else:
+            raise TypeError("4rd arg should be int type")
+
+    def __del__(self):
+        """
+        Destructor, clears dihedral object memory
+        """
+        del self.pkey1
+        del self.pkey2 
+        del self.pkey3 
+        del self.pkey4
+
+    def __str__(self):
+        """
+        'Magic' method for printng contents
+        """
+        return " %s - %s - %s - %s"%(self.pkey1,self.pkey2,self.pkey3,self.pkey4 )
+
